09d8c4c69437253ec9490a0a6d232f9cd35c790b
Alexander Liao <alex.yj.liao@gmail.com>
2018-09-15 15:45:12 -0400

    add original server to hosting to prevent CORB

--- html5up-tessellate/hosting.js
+++ html5up-tessellate/hosting.js
@@ -1,7 +1,47 @@
 var http = require("http");
 var fs = require("fs");
+const clarifai = require("clarifai");
+
+const app = new clarifai.App({
+  apiKey: "b8f4339020aa4524bdf092820a486c3f"
+});
 
 http.createServer(function(request, response) {
+  if (request.url == "/clarifai") {
+    if (request.method == "POST") {
+      var body = "";
+      request.on("data", data => {
+        body += data;
+        if (body.length > 1e9) {
+          request.connection.destroy();
+        }
+      });
+      request.on("end", () => {
+        var data = JSON.parse(body).image;
+        app.models.predict(clarifai.GENERAL_MODEL, {base64: data}).then(
+          predictions => {
+            var concepts = predictions.outputs[0].data.concepts;
+            response.writeHead("200", {"Content-Type": "application/json"});
+            response.write(
+              JSON.stringify(
+                (concepts.filter(a => a.value > 0.9)
+             || [concepts[0]]).map(a => a.name)
+              )
+            );
+            response.end();
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      });
+    } else {
+      response.writeHead("405", {"Content-Type": "text/html"});
+      response.write("405 Method Not Allowed");
+      response.end();
+    }
+    return;
+  }
   var url = request.url.slice(1) || "index.html";
   fs.readFile(url.split("?")[0], (error, data) => {
     if (data === undefined) {
