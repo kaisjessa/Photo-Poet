05deb91f9e844c0acc8505ca25d1e83c91e18256
kaisjessa <kaisjessa@gmail.com>
2018-09-16 03:42:47 -0400

    medium model 8

--- python_model_training/hosting.py
+++ python_model_training/hosting.py
@@ -7,8 +7,7 @@ CORS(app)
 
 @app.route("/", methods = ["POST"])
 def serveRoot():
-    data = request.json()
-    return subprocess.check_output(["python3", "model_testing.py", json.dumps(data)])
+    return subprocess.check_output(["python3", "model_testing.py"])
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000)
