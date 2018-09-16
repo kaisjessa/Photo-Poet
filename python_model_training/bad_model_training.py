0f6f7eb977254f88c20d0ad7ed972767950660d9
kaisjessa <kaisjessa@gmail.com>
2018-09-15 21:46:52 -0400

    created bad model for running locally

--- python_model_training/bad_model_training.py
+++ python_model_training/bad_model_training.py
@@ -16,12 +16,12 @@ with open('./python_model_training/models/training_data.pkl', 'rb') as f:
 model = Sequential()
 #add long short term memory cell
 #output is 400 units (length), input_shape is (number_of_inputs, input_length), return full sequence
-model.add(LSTM(700, input_shape=(X_2.shape[1], X_2.shape[2]), return_sequences=True)) #layer 1
+model.add(LSTM(400, input_shape=(X_2.shape[1], X_2.shape[2]), return_sequences=True)) #layer 1
 #account for overfitting
 model.add(Dropout(0.2))
 
 #add another LSTM layer
-model.add(LSTM(700)) #layer 2
+model.add(LSTM(400)) #layer 2
 #account for overfitting
 model.add(Dropout(0.2))
 
@@ -30,7 +30,7 @@ model.add(Dense(y_2.shape[1], activation='softmax')) #layer 3
 #minimize loss
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 #train the model
-model.fit(X_2, y_2, epochs=1, batch_size=50)
+model.fit(X_2, y_2, epochs=1, batch_size=100)
 #save the model
 model.save("./python_model_training/models/test_model.h5")
 print("Training Completed!")
