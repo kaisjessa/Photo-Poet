a760ecaa0b6751c058ef51e3505781c724dbfbc3
Alexander Liao <alex.yj.liao@gmail.com>
2018-09-16 02:51:25 -0400

    fix a hosting thing and add a python hosting thing for a second layer of interfacing

--- html5up-tessellate/hosting.js
+++ html5up-tessellate/hosting.js
@@ -1,69 +1,71 @@
-const http = require("http");
-const fs = require("fs");
-const clarifai = require("clarifai");
+(async () => {
+  const http = require("http");
+  const fs = require("fs");
+  const clarifai = require("clarifai");
 
-const app = new clarifai.App({
-  apiKey: "b8f4339020aa4524bdf092820a486c3f"
-});
-
-http.createServer(function(request, response) {
-  if (request.url == "/clarifai") {
-    if (request.method == "POST") {
-      var body = "";
-      request.on("data", data => {
-        body += data;
-        if (body.length > 1e9) {
-          request.connection.destroy();
-        }
-      });
-      request.on("end", () => {
-        var data = JSON.parse(body).image;
-        app.models.predict(clarifai.GENERAL_MODEL, {base64: data}).then(
-          predictions => {
-            var concepts = predictions.outputs[0].data.concepts;
-            var filtered = concepts.filter(a => a.value > 0.9);
-            if (filtered.length == 0) filtered = [concepts[0]];
-            concepts = filtered.slice(0, 5);
-            response.writeHead("200", {"Content-Type": "application/json"});
-            response.write(JSON.stringify(concepts.map(a => a.name)));
-            response.end();
-          },
-          error => {
-            response.writeHead("500", {"Content-Type": "text/html"});
-            response.write(JSON.stringify({
-              code: error.data.status.code,
-              desc: error.data.status.description,
-              outputs: error.data.status.outputs
-            }));
-            response.end();
-          }
-        );
-      });
-    } else {
-      response.writeHead("405", {"Content-Type": "text/html"});
-      response.write("405 Method Not Allowed");
-      response.end();
-    }
-    return;
-  }
-  var url = request.url.slice(1) || "index.html";
-  fs.readFile(url.split("?")[0], (error, data) => {
-    if (data === undefined) {
-      response.writeHead(404, {"Content-Type": "text/html"});
-      response.write("404 Not Found");
-      response.end();
-    } else {
-      if (request.url.endsWith("css")) {
-        response.writeHead(200, {"Content-Type": "text/css"});
-      } else if (request.url.endsWith("html")) {
-        response.writeHead(200, {"Content-Type": "text/html"});
-      } else if (request.url.endsWith("js")) {
-        response.writeHead(200, {"Content-Type": "text/js"});
-      } else {
-        response.writeHead(200, {"Content-Type": "text/html"});
-      }
-      response.write(data);
-      response.end();
-    }
+  const app = new clarifai.App({
+    apiKey: "b8f4339020aa4524bdf092820a486c3f"
   });
-}).listen(8000);
+
+  http.createServer(function(request, response) {
+    if (request.url == "/clarifai") {
+      if (request.method == "POST") {
+        var body = "";
+        request.on("data", data => {
+          body += data;
+          if (body.length > 1e9) {
+            request.connection.destroy();
+          }
+        });
+        request.on("end", () => {
+          var data = JSON.parse(body).image;
+          app.models.predict(clarifai.GENERAL_MODEL, {base64: data}).then(
+            predictions => {
+              var concepts = predictions.outputs[0].data.concepts;
+              var filtered = concepts.filter(a => a.value > 0.9);
+              if (filtered.length == 0) filtered = [concepts[0]];
+              concepts = filtered.slice(0, 5);
+              response.writeHead("200", {"Content-Type": "application/json"});
+              response.write(JSON.stringify(concepts.map(a => a.name)));
+              response.end();
+            },
+            error => {
+              response.writeHead("500", {"Content-Type": "text/html"});
+              response.write(JSON.stringify({
+                code: error.data.status.code,
+                desc: error.data.status.description,
+                outputs: error.data.status.outputs
+              }));
+              response.end();
+            }
+          );
+        });
+      } else {
+        response.writeHead("405", {"Content-Type": "text/html"});
+        response.write("405 Method Not Allowed");
+        response.end();
+      }
+      return;
+    }
+    var url = request.url.slice(1) || "index.html";
+    fs.readFile(url.split("?")[0], (error, data) => {
+      if (data === undefined) {
+        response.writeHead(404, {"Content-Type": "text/html"});
+        response.write("404 Not Found");
+        response.end();
+      } else {
+        if (request.url.endsWith("css")) {
+          response.writeHead(200, {"Content-Type": "text/css"});
+        } else if (request.url.endsWith("html")) {
+          response.writeHead(200, {"Content-Type": "text/html"});
+        } else if (request.url.endsWith("js")) {
+          response.writeHead(200, {"Content-Type": "text/js"});
+        } else {
+          response.writeHead(200, {"Content-Type": "text/html"});
+        }
+        response.write(data);
+        response.end();
+      }
+    });
+  }).listen(8000);
+})();
---
+++ python_model_training/hosting.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from flask_cors import CORS
+import subprocess, json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods = ["POST"])
+def serveRoot():
+    data = request.json()
+    return subprocess.check_output(["python3", "model_testing.py", json.dumps(data)])
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000)
