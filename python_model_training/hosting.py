0279c5efea8884002efc50b78cae6a7f67bacf6c
Alexander Liao <alex.yj.liao@gmail.com>
2018-09-16 08:03:10 -0400

    catch errors

--- python_model_training/hosting.py
+++ python_model_training/hosting.py
@@ -1,14 +1,17 @@
 from flask import Flask, request
 from flask_cors import CORS
-import subprocess, json
+import subprocess, json, traceback
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods = ["POST"])
 def serveRoot():
-    data = json.loads(request.data)
-    return subprocess.check_output(["python3", "model_testing.py", data["keyword"]])
+    try:
+        data = json.loads(request.data)
+        return subprocess.check_output(["python3", "model_testing.py", data["keyword"]])
+    except e:
+        return traceback.format_exc()
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000)
