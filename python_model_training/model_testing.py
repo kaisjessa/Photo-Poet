7aae2bc4dd6c6359829b9bdcdf90beadb36b8090
kaisjessa <kaisjessa@gmail.com>
2018-09-16 02:55:37 -0400

    medium model 5

--- python_model_training/model_testing.py
+++ python_model_training/model_testing.py
@@ -10,43 +10,53 @@ from keras.utils import np_utils
 from data_preprocessing import *
 
 #load model
-model = keras.models.load_model("models/medium_model_004.h5")
+model = keras.models.load_model("models/medium_model_005.h5")
 
 model_json = model.to_json()
 with open("models/model.json", "w") as f:
     f.write(model_json)
 
 
-#take random line of integer training data as starting input
-int_train = X[random.randint(0, len(X))]
-
-#convert training data back to array of chars
-chars_array = [int_to_char[n] for n in int_train]
-starting_text = '\n' + ''.join(chars_array) + '\n'
 
 
-#number of characters to generate
-for i in range(400):
-    #reshape data to feed to NN
-    x = np.reshape(int_train, (1, len(int_train), 1))
-    #normalize for NN
-    x = x / float(len(chars))
+keyword = ""
+def check_model(keyword):
+    #take random line of integer training data as starting input
+    int_train = X[random.randint(0, len(X))]
+    '''
+    int_train = int_train[:len(keyword)]
+    for c in keyword:
+        int_train.append(char_to_int[c])
+    print(len(int_train))
+    '''
+    #convert training data back to array of chars
+    chars_array = [int_to_char[n] for n in int_train]
+    #print(chars_array)
+    starting_text = '\n' + ''.join(chars_array) + '\n'
 
-    #the prediction is the index of the next character index
-    #argmax takes the highest number in the onehot array
-    int_prediction = np.argmax(model.predict(x, verbose=0))
-    #print(model.predict(x, verbose=0))
+    #number of characters to generate
+    for i in range(400):
+        #reshape data to feed to NN
+        x = np.reshape(int_train, (1, len(int_train), 1))
+        #normalize for NN
+        x = x / float(len(chars))
 
-    #append prediction to string array for output
-    chars_array.append(int_to_char[int_prediction])
+        #the prediction is the index of the next character index
+        #argmax takes the highest number in the onehot array
+        int_prediction = np.argmax(model.predict(x, verbose=0))
+        #print(model.predict(x, verbose=0))
 
-    #append index to index array
-    int_train.append(int_prediction)
-    #drop first element for next iteration
-    int_train = int_train[1:len(int_train)]
+        #append prediction to string array for output
+        chars_array.append(int_to_char[int_prediction])
 
-predicted_text = ""
-for c in chars_array[100:]:
-    predicted_text += c
-print("Starting text: ", starting_text)
-print("Predicted text: ", predicted_text)
+        #append index to index array
+        int_train.append(int_prediction)
+        #drop first element for next iteration
+        int_train = int_train[1:len(int_train)]
+
+    predicted_text = ""
+    for c in chars_array[100:]:
+        predicted_text += c
+    print("Starting text: ", starting_text)
+    print("Predicted text: ", predicted_text)
+check_model(keyword)
