1507a23f941ea33a857fc3b94e4fb30d20e5085d
Kais Jessa <kaisjessa@gmail.com>
2018-09-21 21:29:47 -0400

    small changes

--- python_model_training/model_training.py
+++ python_model_training/model_training.py
@@ -1,13 +1,12 @@
 #imports
 import numpy as np
-import pandas as pd
 from keras.callbacks import ModelCheckpoint
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.utils import np_utils
-from data_preprocessing import *
+from char_data_preprocessing import *
 #Keras NN
 model = Sequential()
 #add long short term memory cell
